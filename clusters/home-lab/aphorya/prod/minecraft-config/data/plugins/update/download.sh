819804ae118c52d17bc4df616310674285a92650
Julien DOCHE <julien.doche@datadoghq.com>
2022-10-14 00:59:10 +0200

    Add other plugins
    Signed-off-by: Julien DOCHE <julien.doche@datadoghq.com>

--- clusters/home-lab/aphorya/prod/minecraft-config/data/plugins/update/download.sh
+++ clusters/home-lab/aphorya/prod/minecraft-config/data/plugins/update/download.sh
@@ -3,4 +3,12 @@
 set -xe
 
 curl -L -o multiverse-core.jar "https://dev.bukkit.org/projects/multiverse-core/files/3462546/download" # 4.3.1
+curl -L -o multiverse-inventories.jar "https://dev.bukkit.org/projects/multiverse-inventories/files/3687469/download" # 4.2.3
+curl -L -o DiscordSRV.jar "https://github.com/DiscordSRV/DiscordSRV/releases/download/v1.26.0/DiscordSRV-Build-1.26.0.jar" # 1.26.0
+curl -L -o LuckPerms.jar "https://download.luckperms.net/1456/bukkit/loader/LuckPerms-Bukkit-5.4.49.jar" # 5.4.49
+curl -L -o minecraft-prometheus-exporter.jar "https://github.com/sladkoff/minecraft-prometheus-exporter/releases/download/v2.5.0/minecraft-prometheus-exporter-2.5.0.jar" # 2.5.0
+curl -L -o ServerEssentials-Bukkit.jar "https://dev.bukkit.org/projects/server-essentials-for-bukkit/files/4009387/download" # 1.81.4
+curl -L -o worldedit.jar "https://dev.bukkit.org/projects/worldedit/files/3922624/download" # 7.2.12
+curl -L -o worldguard.jar "https://dev.bukkit.org/projects/worldguard/files/3903109/download" # 7.0.8 beta 1
 # curl -L -o NametagEdit.jar "https://www.spigotmc.org/resources/nametagedit.3836/download?version=464805" # 4.5.11
+# curl -L -o MyCommand.jar "https://www.spigotmc.org/resources/mycommand.22272/download?version=468336" # 5.7.3
