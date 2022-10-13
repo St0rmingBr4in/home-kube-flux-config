f803420bfcd3e2bfc04373c10f7efaa88f517d85
Julien DOCHE <julien.doche@datadoghq.com>
2022-10-13 23:56:47 +0200

    Bump nametagedit to 4.5.11
    Signed-off-by: Julien DOCHE <julien.doche@datadoghq.com>

--- clusters/home-lab/aphorya/prod/minecraft-config/data/plugins/update/download.sh
+++ clusters/home-lab/aphorya/prod/minecraft-config/data/plugins/update/download.sh
@@ -3,3 +3,4 @@
 set -xe
 
 curl -L -o multiverse-core.jar "https://dev.bukkit.org/projects/multiverse-core/files/3462546/download" # 4.3.1
+# curl -L -o NametagEdit.jar "https://www.spigotmc.org/resources/nametagedit.3836/download?version=464805" # 4.5.11
