69ea0b247c834959133c4149ff3c9b69be410a16
Julien DOCHE <julien.doche@datadoghq.com>
2023-09-19 21:11:27 +0200

    Add gcp instance
    Signed-off-by: Julien DOCHE <julien.doche@datadoghq.com>

---
+++ clusters/home-lab/terraform/@provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("creds.json") # TODO: sops provider
+
+  project = "driven-current-399517"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "vpc-clusterv2"
+}
