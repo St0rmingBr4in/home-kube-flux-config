3429ae82808c4a0ed82b0c2881f20182fd657620
Julien DOCHE <julien.doche@datadoghq.com>
2023-10-15 02:39:37 +0200

    Move to subfolders
    Signed-off-by: Julien DOCHE <julien.doche@datadoghq.com>

--- clusters/home-lab/cdk8s_definitions/main.py
+++ clusters/home-lab/cdk8s_definitions/main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import cdk8s_plus_27 as kplus
-import cdk8s
 import yaml
-from cdk8s import App, Chart, Names, ApiObject, Helm
+from cdk8s import App, Chart, Names, ApiObject, Helm, Yaml
 from constructs import Construct
 
+KUSTOMIZE_API_VERSION="kustomize.config.k8s.io/v1beta1"
 
 def helm_repo(repo_name):
     if repo_name == "truecharts":
@@ -12,7 +12,9 @@ def helm_repo(repo_name):
 
 
 class HelmChart(Chart):
-    def __init__(self, scope: Construct, resource_id: str, repo_name, version, namespace, values_dir):
+    def __init__(
+        self, scope: Construct, resource_id: str, repo_name, values_dir, **kwargs
+    ):
         super().__init__(scope, resource_id, disable_resource_name_hashes=True)
 
         with open(f"{values_dir}/{resource_id}-values.yaml", "r") as file:
@@ -24,39 +26,70 @@ class HelmChart(Chart):
                 release_name=resource_id,
                 repo=helm_repo(repo_name),
                 chart=resource_id,
-                version=version,
                 values=values,
-                namespace=namespace,
+                **kwargs,
             )
 
 
-app = App(record_construct_metadata=True)
+class HelmApp(App):
+    def __init__(self, name, **kwargs):
+        super().__init__(outdir=f"dist/{name}")
+        self.name = name
 
-HelmChart(
-    scope=app,
-    resource_id="flaresolverr",
-    repo_name="truecharts",
-    version="10.0.5",
-    namespace="prod-media",
-    values_dir="../media"
+        HelmChart(scope=self, resource_id=name, **kwargs)
+
+    def get_name(self):
+        return self.name
+
+    def synth(self):
+        super().synth()
+
+        Yaml.save(
+            file_path=f"dist/{self.name}/kustomization.yaml",
+            docs=[
+                {
+                    "apiVersion": KUSTOMIZE_API_VERSION,
+                    "kind": "Kustomization",
+                    "resources": [f"{self.name}.k8s.yaml"],
+                }
+            ],
+        )
+
+
+apps = [
+    HelmApp(
+        name="flaresolverr",
+        repo_name="truecharts",
+        version="10.0.5",
+        namespace="prod-media",
+        values_dir="../media",
+    ),
+    HelmApp(
+        name="radarr",
+        repo_name="truecharts",
+        version="17.0.7",
+        namespace="prod-media",
+        values_dir="../media",
+    ),
+    HelmApp(
+        name="sonarr",
+        repo_name="truecharts",
+        version="16.0.2",
+        namespace="prod-media",
+        values_dir="../media",
+    ),
+]
+
+for app in apps:
+    app.synth()
+
+Yaml.save(
+    file_path="kustomization.yaml",
+    docs=[
+        {
+            "apiVersion": KUSTOMIZE_API_VERSION,
+            "kind": "Kustomization",
+            "resources": [f"dist/{app.get_name()}" for app in apps],
+        }
+    ],
 )
-
-HelmChart(
-    scope=app,
-    resource_id="radarr",
-    repo_name="truecharts",
-    version="17.0.7",
-    namespace="prod-media",
-    values_dir="../media"
-)
-
-HelmChart(
-    scope=app,
-    resource_id="sonarr",
-    repo_name="truecharts",
-    version="16.0.2",
-    namespace="prod-media",
-    values_dir="../media"
-)
-
-app.synth()
