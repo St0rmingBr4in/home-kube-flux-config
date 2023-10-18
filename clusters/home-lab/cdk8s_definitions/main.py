783d508ad64b9ac59cea1a7da2a0644ff15f4303
Julien DOCHE <julien.doche@gmail.com>
2023-10-18 22:45:03 +0200

    Add external-service for aphorya azuriom
    Signed-off-by: Julien DOCHE <julien.doche@gmail.com>

--- clusters/home-lab/cdk8s_definitions/main.py
+++ clusters/home-lab/cdk8s_definitions/main.py
@@ -54,11 +54,15 @@ class KustomizationChart(Chart):
 
 class HelmApp(App):
     def __init__(self, name, values_dir, **kwargs):
-        super().__init__(outdir=f"dist/{name}")
-        self.name = name
-
         with open(f"{values_dir}/{name}.yaml", "r") as helm_release_file:
             helm_release = yaml.safe_load(helm_release_file)
+            namespace = helm_release["spec"]["targetNamespace"]
+
+            self.name = name
+            self.namespace = namespace
+
+            super().__init__(outdir=f"dist/{namespace}/{name}")
+
             with open(f"{values_dir}/{name}-values.yaml", "r") as values_file:
                 values = yaml.safe_load(values_file)
 
@@ -71,28 +75,28 @@ class HelmApp(App):
                     repo=helm_repo(
                         helm_release["spec"]["chart"]["spec"]["sourceRef"]["name"]
                     ),
-                    namespace=helm_release["spec"]["targetNamespace"],
+                    namespace=namespace,
                     **kwargs,
                 )
 
-        KustomizationChart(
-            scope=self,
-            identifier=f"{name}-flux-kustomization",
-            metadata={"namespace": "flux-system", "name": name},
-            spec={
-                "interval": "10m0s",
-                "path": f"./clusters/home-lab/cdk8s_definitions/dist/{name}",
-                "prune": True,
-                "decryption": {
-                    "provider": KustomizationSpecDecryptionProvider.SOPS,
-                    "secret_ref": {"name": "sops-gpg"},
+            KustomizationChart(
+                scope=self,
+                identifier=f"{name}-flux-kustomization",
+                metadata={"namespace": "flux-system", "name": name},
+                spec={
+                    "interval": "10m0s",
+                    "path": f"./clusters/home-lab/cdk8s_definitions/dist/{namespace}/{name}",
+                    "prune": True,
+                    "decryption": {
+                        "provider": KustomizationSpecDecryptionProvider.SOPS,
+                        "secret_ref": {"name": "sops-gpg"},
+                    },
+                    "source_ref": {
+                        "kind": KustomizationSpecSourceRefKind.GIT_REPOSITORY,
+                        "name": "flux-system",
+                    },
                 },
-                "source_ref": {
-                    "kind": KustomizationSpecSourceRefKind.GIT_REPOSITORY,
-                    "name": "flux-system",
-                },
-            },
-        )
+            )
 
     def get_name(self):
         return self.name
@@ -101,7 +105,7 @@ class HelmApp(App):
         super().synth()
 
         Yaml.save(
-            file_path=f"dist/{self.name}/kustomization.yaml",
+            file_path=f"dist/{self.namespace}/{self.name}/kustomization.yaml",
             docs=[
                 {
                     "apiVersion": KUSTOMIZE_API_VERSION,
@@ -177,6 +181,10 @@ apps = [
         name="cloudnative-pg",
         values_dir="../cloudnative-pg",
     ),
+    HelmApp(
+        name="external-service",
+        values_dir="../aphorya/prod",
+    ),
 ]
 
 for app in apps:
@@ -189,7 +197,7 @@ Yaml.save(
             "apiVersion": KUSTOMIZE_API_VERSION,
             "kind": "Kustomization",
             "resources": [
-                f"dist/{app.get_name()}/{app.get_name()}-flux-kustomization.k8s.yaml"
+                f"dist/{app.namespace}/{app.get_name()}/{app.get_name()}-flux-kustomization.k8s.yaml"
                 for app in apps
             ],
         }
